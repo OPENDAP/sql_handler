8296b504945d6901bca1b9835bd71cfce097c71e
James Gallagher <jgallagher@opendap.org>
2012-08-29 23:16:54 +0000

    Fixed syntax for MySQL

--- install.dir/sqlh.sql
+++ install.dir/sqlh.sql
@@ -1,25 +1,13 @@
-CREATE TABLE "sqlh_table"(
-  "int" integer NOT NULL DEFAULT 0,
-  "real" real NOT NULL DEFAULT 0,
-  "varchar" character varying(50),
-  CONSTRAINT pk PRIMARY KEY ("int")
-);
-ALTER TABLE sqlh_table OWNER TO postgres;
-COMMENT ON COLUMN sqlh_table."int" IS 'comment';
-COMMENT ON COLUMN sqlh_table."real" IS 'comment ''real''
-';
+USE test;
+CREATE TABLE sqlh_table 
+(a integer NOT NULL DEFAULT 0,
+b real NOT NULL DEFAULT 0,
+c varchar(50),
+CONSTRAINT pk PRIMARY KEY (a));
+ALTER TABLE sqlh_table OWNER TO mysql;
+COMMENT ON COLUMN sqlh_table.a IS 'comment';
+COMMENT ON COLUMN sqlh_table.b IS 'comment ''real''';
 
-INSERT INTO sqlh_table("int", "real", "varchar") VALUES 
-	('1', '81.0', 'string_a'),
-	('2', '61.1', 'string_b'),
-	('3', '51.0', 'string_c'),
-	('4', '2100.0', 'string_d'),
-	('5', '21.0', 'string_e'),
-	('6', '4133.0', 'string_f'),
-	('7', '31.4', 'string_g'),
-	('8', '21.3', 'string_h'),
-	('9', '11.6', 'string_i'),
-	('10', '22.2', 'string_j')
-    ;
+INSERT INTO sqlh_table(a, b, c) VALUES ('1', '81.0', 'string_a'), ('2', '61.1', 'string_b'), ('3', '51.0', 'string_c'), ('4', '2100.0', 'string_d'), ('5', '21.0', 'string_e'), ('6', '4133.0', 'string_f'), ('7', '31.4', 'string_g'), ('8', '21.3', 'string_h'), ('9', '11.6', 'string_i'), ('10', '22.2', 'string_j');
 \commit
 \quit
